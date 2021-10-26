576e11f0d617366d555f51e516ef69c0ef5f8421
Zhangqr <ricey54560@gmail.com>
2021-10-26 10:51:13 +0800

    0307 per material

--- Assets/03-06 ShaderGUI/Scripts/Example/MeshBall.cs
+++ Assets/03-06 ShaderGUI/Scripts/Example/MeshBall.cs
@@ -10,11 +10,20 @@ namespace NiuBiSRP
     public class MeshBall : MonoBehaviour
     {
         static int baseColorID = Shader.PropertyToID("_BaseColor");
+        static int metallicId = Shader.PropertyToID("_Metallic");
+        static int smoothnessId = Shader.PropertyToID("_Smoothness");
+        static int clipId = Shader.PropertyToID("_Clip");
         [SerializeField] private Mesh mesh;
         [SerializeField] private Material material;
 
         private Matrix4x4[] matrices = new Matrix4x4[1023];
         private Vector4[] colorList = new Vector4[1023];
+
+        private float[] metallicList = new float[1023],
+            smoothnessList = new float[1023],
+            clipList = new float[1023];
+        // private float[] smoothnessList = new float[1023];
+        // private float[] clipList = new float[1023];
         private static MaterialPropertyBlock block;
 
         private void Awake()
@@ -29,15 +38,25 @@ namespace NiuBiSRP
                 matrices[i] = Matrix4x4.TRS(
                     Random.insideUnitSphere * 10,
                     Quaternion.Euler(Random.value*360,Random.value*360,Random.value*360),
-                    Random.Range(0.2f,0.6f)* Vector3.one);
+                    Random.Range(0.5f,1.3f)* Vector3.one);
                 
                 colorList[i] = new Vector4(Random.value, Random.value, Random.value, 1.0f);
+                colorList[i] *= 0.5f;
+                colorList[i] += Vector4.one * 0.5f;
+                colorList[i].w = 1.0f;
+                // metallicList[i] = Random.value < 0.25f ? 0.0f : 1.0f;
+                metallicList[i] = Random.value;
+                smoothnessList[i] = Random.Range(0.05f,0.95f);
+                clipList[i] = Random.value;
             }
         }
 
         private void Update()
         {
             block.SetVectorArray(baseColorID,colorList);
+            block.SetFloatArray(metallicId,metallicList);
+            block.SetFloatArray(smoothnessId,smoothnessList);
+            block.SetFloatArray(clipId,clipList);
             Graphics.DrawMeshInstanced(mesh,0,material,matrices,1023,block);
         }
     }
--- Assets/03-06 ShaderGUI/Scripts/Example/PerObjectMaterialProperties.cs
+++ Assets/03-06 ShaderGUI/Scripts/Example/PerObjectMaterialProperties.cs
@@ -10,13 +10,19 @@ namespace NiuBiSRP
     public class PerObjectMaterialProperties : MonoBehaviour
     {
         [SerializeField] Color baseColor;
+        [Range(0, 1),SerializeField] private float metallic;
+        [Range(0, 1),SerializeField] private float smoothness;
+        [Range(0, 1),SerializeField] private float clip;
         static int baseColorId = Shader.PropertyToID("_BaseColor");
+        static int metallicId = Shader.PropertyToID("_Metallic");
+        static int smoothnessId = Shader.PropertyToID("_Smoothness");
+        static int clipId = Shader.PropertyToID("_Clip");
         
         static MaterialPropertyBlock block;
         
         private void Awake()
         {
-            baseColor = new Color(Random.value, Random.value, Random.value);
+            // baseColor = new Color(Random.value, Random.value, Random.value);
             OnValidate();
         }
         
@@ -27,6 +33,9 @@ namespace NiuBiSRP
                 block = new MaterialPropertyBlock();
             }
             block.SetColor(baseColorId,baseColor);
+            block.SetFloat(metallicId,metallic);
+            block.SetFloat(smoothnessId,smoothness);
+            block.SetFloat(clipId,clip);
             GetComponent<Renderer>().SetPropertyBlock(block);
         }
     }
